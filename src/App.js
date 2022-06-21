5c0ca0d40959e5790ee92c2ceefd19f2ceb59359
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-06-21 17:04:09 +0200

    fixat någon form av basdesign

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="background">
+        <div></div>
+          <div className="textArea">
+
+          <form className="form" action="">
+      <input className="inputField" autocomplete="off" />
+      <button className="button">Send</button>
+    </form>
+
+          </div>
+      </div>
+   
   );
 }
 
